d8176eeddd3af07299ddcf40666224b916a361f4
William Viktorsson <william@viktorsson.com>
2022-11-17 01:44:33 +0100

    added comments

--- src/lib/stores.js
+++ src/lib/stores.js
@@ -1,5 +1,13 @@
 import { writable } from "svelte/store";
 
+/* 
+   We are using a writable store from svelte. We are keeping the store in sync with
+   the localStorage in the browser.
+
+   Read about localStorage here: 
+   https://developer.mozilla.org/en-US/docs/Web/API/Window/localStorage
+*/
+
 /* initialize the story_id to '1' if the story_id has not already been stored */
 const story_id =  window.localStorage.getItem('story_id') ?? "1"
 /* https://svelte.dev/tutorial/writable-stores */
