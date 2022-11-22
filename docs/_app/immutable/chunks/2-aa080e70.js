19f5f9987b9f9f898623b58184b125d58c8202a3
William Viktorsson <william@viktorsson.com>
2022-11-22 22:14:46 +0100

    removed prepush

--- docs/_app/immutable/chunks/2-aa080e70.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/pages/_page.svelte-774e46fc.js";export{t as component};
--- docs/_app/immutable/chunks/3-9e6d34cb.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/pages/telltale/_page.svelte-bbc352f3.js";export{t as component};
