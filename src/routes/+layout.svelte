72d3326766cbd33d3b58acaf395fe0c12528cc22
William Viktorsson <william@viktorsson.com>
2022-11-28 00:19:55 +0100

    memory

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,8 @@
 <nav>
   <a href="/">Hem</a>
   <a href="/telltale">Telltale</a>
+  <a href="/memory">Memory</a>
+
   <a
     target="_blank"
     href="https://github.com/williamviktorsson/williamviktorsson.github.io"
---
+++ src/routes/memory/+page.svelte
@@ -0,0 +1,99 @@
+<script>
+  let cards = [];
+  for (let index = 0; index < 4; index++) {
+    cards.push({
+      id: 5, // TODO: unique ids per card card
+      img: "/notrick.gif", // TODO: unique images per card card
+      flipped: false,
+      completed: false,
+    });
+  }
+  let flipcount = 0;
+  function flip(card) {
+    // flip card over if two cards are not already flipped
+    if (flipcount < 2) {
+      // TODO: Probably do what?
+
+      // flip the cards over after 2s from seeing both cards
+      if (flipcount == 2) {
+        setTimeout(() => {
+          // flip over cards that have not been marked as "completed"
+          cards.forEach((card) => {
+            card.flipped = card.completed;
+          });
+          flipcount = 0;
+          cards = cards;
+        }, 2000);
+      }
+      cards = cards;
+    } else {
+      alert("chill");
+    }
+  }
+</script>
+
+<main>
+  <div class="row">
+    {#each cards as card, i}
+      <div
+        on:click={() => {
+          flip(card);
+        }}
+        on:keypress={() => {
+          flip(card);
+        }}
+        class:flipped={card.flipped}
+        class="card"
+      >
+        <img class="front" src={card.img} alt="" />
+        <img class="back" src="front.webp" alt="" />
+      </div>
+    {/each}
+  </div>
+</main>
+
+<style>
+  main {
+    margin-top: 50px;
+    display: flex;
+    place-content: center;
+    place-items: center;
+  }
+
+  .row {
+    display: grid;
+    gap: 20px;
+    grid-template-columns: repeat(3, 100px);
+    grid-template-rows: repeat(3, 100px);
+  }
+
+  .card {
+    border: 1px solid black;
+    cursor: pointer;
+    transition: transform 1s;
+    transform-style: preserve-3d;
+    width: 100%;
+    height: 100%;
+  }
+
+  .card.flipped {
+    transform: rotateY(180deg);
+  }
+
+  .card .back {
+    transform: rotateY(0deg);
+  }
+
+  .card .front {
+    transform: rotateY(180deg);
+  }
+
+  .card img {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+    backface-visibility: hidden;
+    -webkit-backface-visibility: hidden;
+    position: absolute;
+  }
+</style>
