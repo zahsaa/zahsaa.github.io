69dc051d7d64e52059cec074df66ed113aec804b
Zahraa Saadi <zahsaa02@elevad.umea.se>
2023-11-14 13:23:31 +0100

    klar med start

---
+++ docs/_app/immutable/chunks/5-ce8ea438.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/_page.svelte-498fe06c.js";export{t as component};
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,47 @@
 
+
 <h1>
   Välkommen till min hemsida!
 </h1>
 
 <h2>Här har jag samlat min uppgifter jag gjort i samband med webbutveckling i årskurs 2 på Maja Beskowgymnasiet!</h2>
 
-<h3 >Zahraa Efternamn</h3>
+<h3 class="name">Zahraa Efternamn</h3>
+<p id="inspo">
+  "The only way to learn a new programming language is by writing
+  programs in it." - Dennis Ritchie, creator of the C programming language.
+  </p>
+  <p style="background-color:pink">
+  "Every great developer you know got there by solving problems they
+  were unqualified to solve until they actually did it." - Patrick McKenzie,
+  software engineer and entrepreneur.
+  </p>
+  
+ 
+
+<style>
+  h1, h2, h3, p {
+display: block;
+}
+  h1{
+    color:red;
+  }
+  h2{
+    color:black;
+  }
+  h3{
+    color:#05f7ff;
+  }
+
+
+  .name{
+  color: rebeccapurple;
+  font-family:'Gill Sans'
+  }
+  
+#inspo{
+font-family: courier;
+font-size: 160%;
+}
+  </style>
 
